722b37b4b1b042291a6895e9a3a46642c90092d4
BenAytu <aytugbengu2000@gmail.com>
2023-01-22 06:06:46 +0300

    Korku oyunumun Adım Sesleri için yazdığım kod klasöre atıldı

---
+++ FootStep.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStep : MonoBehaviour
+{
+    [SerializeField] AudioClip[] audioClip;
+    public AudioSource aS;
+   
+    private void Step ()
+    {
+        AudioClip clip = GetRandomClip();
+        aS.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        return audioClip[index];
+    }
+}
